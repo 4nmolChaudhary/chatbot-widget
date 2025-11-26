25c9520cf181d1b2dcc6ced281e4a2bc3532ccdb
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2025-11-26 21:50:07 +0530

    [FEAT] updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,16 +10,19 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': {},
+  },
   build: {
     lib: {
       entry: 'src/main.tsx',
       name: 'assistant',
       fileName: 'assistant',
-      formats: ['iife'], // Immediately Invoked Function Expression (runs in browser)
+      formats: ['iife'],
     },
     rollupOptions: {
       output: {
-        // Prevent splitting into multiple chunks
         inlineDynamicImports: true,
       },
     },
